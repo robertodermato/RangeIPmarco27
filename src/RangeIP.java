acc59a1bae6cdd324779ea41df4dc7f7d57a1b0b
Roberto <robertodermato@gmail.com>
2020-03-27 15:54:44 -0300

    duas classes ARN e RangeIP

---
+++ src/RangeIP.java
@@ -0,0 +1,43 @@
+public class RangeIP implements Comparable<RangeIP> {
+        private Integer ipInicial;
+        private Integer ipFinal;
+
+        public RangeIP(Integer ipInicial, Integer ipFinal) {
+            this.ipInicial = ipInicial;
+            this.ipFinal = ipFinal;
+        }
+
+        public Integer getIpInicial() {return ipInicial;}
+
+        public Integer getIpFinal() {return ipFinal;}
+
+        public void setIpInicial(Integer newIpInicial) {
+            this.ipInicial = newIpInicial;
+        }
+
+        public void setIpFinal(Integer newIpFinal) {
+            this.ipFinal = newIpFinal;
+        }
+
+
+        @Override
+        public int compareTo(RangeIP outroRange) {
+            return ipInicial.compareTo(outroRange.getIpInicial());
+        }
+
+
+        @Override
+        public String toString() {
+            return "IP inicial: " + ipInicial + " IP final: " + ipFinal;
+        }
+
+        /*
+    @Override
+    public int compareTo(RangeIP outroRange) {
+            if (ipInicial < outroRange.getIpInicial()) return -1;
+        if (ipInicial == outroRange.getIpInicial()) return 0;
+        else return 1;
+            }
+
+         */
+}
